33a83bde9602525a8cb2eae60666c38ab43475c6
Xavier Capaldi <xcapaldi@scribo.biz>
2020-04-02 14:24:41 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tumbled Braid Knotting Model 
---
+++ tbkm.py
@@ -0,0 +1,21 @@
+# want to modify rates of crossing in given direction
+#                rates of crossing above and below
+#                rates of initial twist
+
+
+
+      ---------
+      |
+------|---------------------------
+      |
+------|--------------------------
+      |     ⌜----⌞⌟
+------|---------------------------
+      |     |
+      -------
+
+
+┃│
+┗━┓
+ │
+
